6c97e59a91c2afef2809009560996910bf9a44a7
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2025-01-09 11:13:02 +0100

    Initial commit

---
+++ APIKlient/Pokemon.cs
@@ -0,0 +1,5 @@
+public class Pokemon
+{
+    public string name {get; set;}
+    public int weight {get; set;}
+}
---
+++ APIKlient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Xml.XPath;
+
+HttpClient client = new();
+
+client.BaseAddress = new("https://pokeapi.co/api/v2/"); //hämtar info
+
+HttpResponseMessage response = client.GetAsync("pokemon/snorlax").Result;
+
+string snorlax = response.Content.ReadAsStringAsync().Result;
+
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(snorlax); //från text till objekt
+
+// slippa två rader över här
+// Pokemon p = response.Content.ReadFromJsonAsync<Pokemon>.Result;
+
+Console.WriteLine(p.weight);
+// File.WriteAllText("snorlax.json", snorlax); //skapa json fil
+
+Console.ReadLine();
